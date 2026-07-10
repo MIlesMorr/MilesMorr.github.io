b77247327378d7afc5cb1aab6722f6925f65aac0
MIlesMorr <MIlesMorr@users.noreply.github.com>
2026-07-10 15:41:11 +0000

    deploy: 3eb2163bc802d48da2ea44b9c717d65ad90c41bc

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1783697948352";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1783698069416";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
